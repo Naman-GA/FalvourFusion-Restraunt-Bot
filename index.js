6b6a165c324e96f3989859ec4deebaa0d46d93fd
Naman-GA <ngmarch15@gmail.com>
2023-09-27 10:41:51 +0530

    notification

--- index.js
+++ index.js
@@ -46,42 +46,38 @@ app.use("/api/messages", async (req, res) => {
 });
 
 app.post("/api/notification", async (req, res) => {
-  try {
-    // Extract the context from the request body (assuming it contains context information)
-
-    // Check if the context object is valid and contains the necessary information
-    if (sharedContext) {
-      const botConversationReference = {
-        bot: { id: sharedContext.activity.recipient.id },
-        channelId: sharedContext.activity.channelId,
-        serviceUrl: sharedContext.activity.serviceUrl,
-        conversation: {
-          id: sharedContext.activity.conversation.id,
-        },
-      };
-
-      const orderNotification = "Hii this is notifiation";
-
-      const message = {
-        type: "message",
-        text: `Order details: ${JSON.stringify(orderNotification)}`,
-      };
-
-      await adapter.continueConversationAsync(
-        botConversationReference,
-        async (innerContext) => {
-          await innerContext.sendActivity(message);
-        }
-      );
-      res.json({ success: true });
-    } else {
-      // Handle the case where the context is not available or lacks necessary information
-      res.status(400).json({ error: "Invalid or incomplete context" });
-    }
-  } catch (error) {
-    console.error("Error sending notification:", error);
-    res.status(500).json({ error: "Error sending notification" });
-  }
+  // try {
+  //   // Extract the context from the request body (assuming it contains context information)
+  //   // Check if the context object is valid and contains the necessary information
+  //   if (sharedContext) {
+  //     const botConversationReference = {
+  //       bot: { id: sharedContext.activity.recipient.id },
+  //       channelId: sharedContext.activity.channelId,
+  //       serviceUrl: sharedContext.activity.serviceUrl,
+  //       conversation: {
+  //         id: sharedContext.activity.conversation.id,
+  //       },
+  //     };
+  //     const orderNotification = "Hii this is notifiation";
+  //     const message = {
+  //       type: "message",
+  //       text: `Order details: ${JSON.stringify(orderNotification)}`,
+  //     };
+  //     await adapter.continueConversationAsync(
+  //       botConversationReference,
+  //       async (innerContext) => {
+  //         await innerContext.sendActivity(message);
+  //       }
+  //     );
+  //     res.json({ success: true });
+  //   } else {
+  //     // Handle the case where the context is not available or lacks necessary information
+  //     res.status(400).json({ error: "Invalid or incomplete context" });
+  //   }
+  // } catch (error) {
+  //   console.error("Error sending notification:", error);
+  //   res.status(500).json({ error: "Error sending notification" });
+  // }
 });
 
 app.listen(3000, () => {
