f84b322f99567fe6f000e945eade6976d5d6f160
Naman-GA <ngmarch15@gmail.com>
2023-09-27 10:35:51 +0530

    notification

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const { AdaptiveCards } = require("@microsoft/adaptivecards-tools");
 const { Bot } = require("./bot");
 const { RootDialog } = require("./Dialogs/rootD");
 const dotenv = require("dotenv");
+const { Context } = require("mocha");
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -35,37 +36,54 @@ const conversationState = new ConversationState(memoryStorage);
 const rootDialoug = new RootDialog(conversationState);
 
 const myBot = new Bot(conversationState, rootDialoug);
-
+let sharedContext = null;
 app.use("/api/messages", async (req, res) => {
   adapter.process(req, res, async (context) => {
+    sharedContext = context;
     await myBot.run(context);
+    console.log(context);
   });
 });
 
-app.post(
-  "/api/notification",
-  // Add more parsers if needed
-  async (req, res) => {
-    // await sendAdaptiveCard(
-    //   AdaptiveCards.declare(notificationTemplate).render({
-    //     title: "New Event Occurred!",
-    //     appName: "Contoso App Notification",
-    //     description: `This is a sample http-triggered notification to ${target.type}`,
-    //     notificationUrl: "https://aka.ms/teamsfx-notification-new",
-    //   })
-    // );
-    async (req, res) => {
-      // Create a TurnContext object using the adapter
-      const context = adapter.createTurnContext(req);
+app.post("/api/notification", async (req, res) => {
+  try {
+    // Extract the context from the request body (assuming it contains context information)
 
-      // Create an instance of your Bot class and run it
-      const myBot = new Bot(conversationState, rootDialoug);
-      await myBot.sendNotification(context);
-      // Respond with a success message
-      res.send("Notification sent.");
-    };
+    // Check if the context object is valid and contains the necessary information
+    if (sharedContext) {
+      const botConversationReference = {
+        bot: { id: sharedContext.activity.recipient.id },
+        channelId: sharedContext.activity.channelId,
+        serviceUrl: sharedContext.activity.serviceUrl,
+        conversation: {
+          id: sharedContext.activity.conversation.id,
+        },
+      };
+
+      const orderNotification = "Hii this is notifiation";
+
+      const message = {
+        type: "message",
+        text: `Order details: ${JSON.stringify(orderNotification)}`,
+      };
+
+      await adapter.continueConversationAsync(
+        botConversationReference,
+        async (innerContext) => {
+          await innerContext.sendActivity(message);
+        }
+      );
+      res.json({ success: true });
+    } else {
+      // Handle the case where the context is not available or lacks necessary information
+      res.status(400).json({ error: "Invalid or incomplete context" });
+    }
+  } catch (error) {
+    console.error("Error sending notification:", error);
+    res.status(500).json({ error: "Error sending notification" });
   }
-);
+});
+
 app.listen(3000, () => {
   console.log("Server Listening on 3000");
 });
