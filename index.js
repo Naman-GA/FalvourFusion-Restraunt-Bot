6fe03953cbad4318469ceaee052a02cd02c7a256
Naman-GA <ngmarch15@gmail.com>
2023-09-27 11:11:56 +0530

    new file

--- index.js
+++ index.js
@@ -45,64 +45,37 @@ app.use("/api/messages", async (req, res) => {
 });
 
 app.post("/api/notification", async (req, res) => {
-  // Create an instance of your Bot class and run it
-  const myBot = new Bot(conversationState, rootDialoug);
-  const notificationCard = {
-    type: "AdaptiveCard",
-    version: "1.0",
-    body: [
-      {
-        type: "TextBlock",
-        text: "Notification",
-        size: "large",
-        weight: "bolder",
-      },
-      {
-        type: "TextBlock",
-        text: "Title",
-        size: "medium",
-        weight: "bolder",
-      },
-      {
-        type: "TextBlock",
-        text: "Your notification title goes here",
-        wrap: true,
-      },
-      {
-        type: "TextBlock",
-        text: "Description",
-        size: "medium",
-        weight: "bolder",
-      },
-      {
-        type: "TextBlock",
-        text: "Your notification description goes here",
-        wrap: true,
-      },
-    ],
-  };
+  try {
+    if (sharedContext) {
+      // Use the sharedContext to construct a conversation reference
+      const conversationReference = TurnContext.getConversationReference(
+        sharedContext.activity
+      );
 
-  // Send the Adaptive Card as a response
-  await context.sendActivity({
-    attachments: [CardFactory.adaptiveCard(notificationCard)],
-  });
+      // You can save the conversation reference to a database or in-memory storage here
 
-  // Respond with a success message
-  res.send("Notification sent.");
-
-  // Respond with a success message
-});
-
-<<<<<<< HEAD
-=======
-      // Respond with a success message
-      res.send("Notification sent.");
-
-      // Respond with a success message
-    };
+      // Later, use the stored reference to send a proactive message
+      await adapter.continueConversationAsync(
+        conversationReference,
+        async (proactiveContext) => {
+          // Construct and send the notification message
+          await proactiveContext.sendActivity({
+            type: "message",
+            text: "This is a proactive notification.",
+          });
+        }
+      );
+      res.json({ success: true });
+    } else {
+      // Handle the case where the context is not available or lacks necessary information
+      res.status(400).json({ error: "Invalid or incomplete context" });
+    }
+  } catch (error) {
+    console.error("Error sending notification:", error);
+    res.status(500).json({ error: "Error sending notification" });
   }
-);
->>>>>>> parent of 74b5f1c (notification)
+});
+// Respond with a success message
 app.listen(3000, () => {
   console.log("Server Listening on 3000");
 });
